c631ec597d8d2398e841f988ab927e05ea5023a1
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2024-07-04 21:41:27 +0200

    fix: remove unused files
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- Tests/Sources/go-feature-flag-provider/model/bulk_evaluation_status.swift
+++
@@ -1,5 +0,0 @@
-enum BulkEvaluationStatus {
-    case success_no_changes
-    case success_with_changes
-    case rate_limited
-}
--- Tests/Sources/go-feature-flag-provider/model/options.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct GoFeatureFlagProviderOptions {
-    let endpoint: String
-    var pollInterval: TimeInterval = 30
-    var networkService: NetworkingService? = URLSession.shared
-}
