0fe8aebc06ccc873fbf5ae4506d958a5d7d38412
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-01-20 18:31:06 +0100

    feat: Exporter Metadata
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

---
+++ Sources/GOFeatureFlag/controller/exporter_metadata.swift
@@ -0,0 +1,82 @@
+import Foundation
+import OpenFeature
+
+// Define a Codable enum that can represent any type of JSON value
+public enum ExporterMetadataValue: Codable, Equatable {
+    case string(String)
+    case integer(Int64)
+    case double(Double)
+    case bool(Bool)
+
+    // Decode the JSON based on its type
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let stringValue = try? container.decode(String.self) {
+            self = .string(stringValue)
+        } else if let intValue = try? container.decode(Int64.self) {
+            self = .integer(intValue)
+        } else if let doubleValue = try? container.decode(Double.self) {
+            self = .double(doubleValue)
+        } else if let boolValue = try? container.decode(Bool.self) {
+            self = .bool(boolValue)
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot decode JSONValue")
+        }
+    }
+
+    // Encode the JSON based on its type
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .string(let value):
+            try container.encode(value)
+        case .integer(let value):
+            try container.encode(value)
+        case .double(let value):
+            try container.encode(value)
+        case .bool(let value):
+            try container.encode(value)
+        }
+    }
+
+    public func asString() -> String? {
+        if case .string(let value) = self {
+            return value
+        }
+        return nil
+    }
+
+    public func asBoolean() -> Bool? {
+        if case .bool(let value) = self {
+            return value
+        }
+        return nil
+    }
+
+    public func asInteger() -> Int64? {
+        if case .integer(let value) = self {
+            return value
+        }
+        return nil
+    }
+
+    public func asDouble() -> Double? {
+        if case .double(let value) = self {
+            return value
+        }
+        return nil
+    }
+
+    public func toValue() -> Value {
+            switch self {
+            case .string(let string):
+                return .string(string)
+            case .integer(let integer):
+                return .integer(integer)
+            case .double(let double):
+                return .double(double)
+            case .bool(let bool):
+                return .boolean(bool)
+            }
+        }
+}
--- Tests/GOFeatureFlagTests/provider_tests.swift
+++ Tests/GOFeatureFlagTests/provider_tests.swift
@@ -17,6 +17,7 @@ class GoFeatureFlagProviderTests: XCTestCase {
             options: GoFeatureFlagProviderOptions(
                 endpoint: "https://localhost:1031",
                 dataFlushInterval: 1,
+                exporterMetadata: ["version": ExporterMetadataValue.string("1.0.0")],
                 networkService: mockNetworkService
             )
         )
@@ -34,11 +35,72 @@ class GoFeatureFlagProviderTests: XCTestCase {
 
         let expectation = self.expectation(description: "Waiting for delay")
 
-        DispatchQueue.global().asyncAfter(deadline: .now() + 1.0) { expectation.fulfill() }
-        await fulfillment(of: [expectation], timeout: 2.0)
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2.0) { expectation.fulfill() }
+        await fulfillment(of: [expectation], timeout: 3.0)
 
         XCTAssertEqual(1, mockNetworkService.dataCollectorCallCounter)
         XCTAssertEqual(6, mockNetworkService.dataCollectorEventCounter)
+        
+        do {
+            let httpBodyCollector = mockNetworkService.requests[mockNetworkService.requests.count - 1].httpBody!
+            // print httpBodyCollector
+            print(String(data: httpBodyCollector, encoding: .utf8)!)
+            let json = try JSONSerialization.jsonObject(with: httpBodyCollector, options: []) as? [String: Any]
+            guard let jsonDict = json else {
+                XCTFail("Could not deserialize JSON")
+                return
+            }
+        } catch {
+            XCTFail("Error deserializing JSON: \(error)")
+        }
+        
+    }
+    
+    func testExporterMetadata() async {
+        let mockNetworkService = MockNetworkingService(mockStatus: 200)
+        let provider = GoFeatureFlagProvider(
+            options: GoFeatureFlagProviderOptions(
+                endpoint: "https://localhost:1031",
+                dataFlushInterval: 1,
+                exporterMetadata: ["version": ExporterMetadataValue.string("1.0.0"),"testInt": ExporterMetadataValue.integer(123), "testDouble": ExporterMetadataValue.double(123.45)],
+                networkService: mockNetworkService
+            )
+        )
+        let evaluationCtx = MutableContext(targetingKey: "ede04e44-463d-40d1-8fc0-b1d6855578d0")
+        let api = OpenFeatureAPI()
+        await api.setProviderAndWait(provider: provider, initialContext: evaluationCtx)
+        let client = api.getClient()
+
+        _ = client.getBooleanDetails(key: "my-flag", defaultValue: false)
+        _ = client.getBooleanDetails(key: "my-flag", defaultValue: false)
+        _ = client.getIntegerDetails(key: "int-flag", defaultValue: 1)
+        _ = client.getDoubleDetails(key: "double-flag", defaultValue: 1.0)
+        _ = client.getStringDetails(key: "string-flag", defaultValue: "default")
+        _ = client.getObjectDetails(key: "object-flag", defaultValue: Value.null)
+
+        let expectation = self.expectation(description: "Waiting for delay")
+
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2.0) { expectation.fulfill() }
+        await fulfillment(of: [expectation], timeout: 3.0)
+
+        XCTAssertEqual(1, mockNetworkService.dataCollectorCallCounter)
+        XCTAssertEqual(6, mockNetworkService.dataCollectorEventCounter)
+        
+        do {
+            let httpBodyCollector = mockNetworkService.requests[mockNetworkService.requests.count - 1].httpBody!
+            let decodedStruct = try JSONDecoder().decode(DataCollectorRequest.self, from: httpBodyCollector)
+            let want = [
+                "version": ExporterMetadataValue.string("1.0.0"),
+                "testDouble": ExporterMetadataValue.double(123.45),
+                "testInt": ExporterMetadataValue.integer(123),
+                "openfeature": ExporterMetadataValue.bool(true),
+                "provider": ExporterMetadataValue.string("swift")
+            ] as? [String: ExporterMetadataValue]
+            XCTAssertEqual(want, decodedStruct.meta)
+        } catch {
+            XCTFail("Error deserializing: \(error)")
+        }
+        
     }
 
     func testProviderMultipleHookCall() async {
