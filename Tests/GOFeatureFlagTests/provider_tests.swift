2248e58cbced7acf32db80c003b691ba934b0693
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-01-21 13:50:34 +0100

    adding test with exporter metadata nil
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- Tests/GOFeatureFlagTests/provider_tests.swift
+++ Tests/GOFeatureFlagTests/provider_tests.swift
@@ -85,7 +85,49 @@ class GoFeatureFlagProviderTests: XCTestCase {
         } catch {
             XCTFail("Error deserializing: \(error)")
         }
+    }
+    
+    func testExporterMetadataNil() async {
+        let mockNetworkService = MockNetworkingService(mockStatus: 200)
+        let provider = GoFeatureFlagProvider(
+            options: GoFeatureFlagProviderOptions(
+                endpoint: "https://localhost:1031",
+                dataFlushInterval: 1,
+                exporterMetadata: nil,
+                networkService: mockNetworkService
+            )
+        )
+        let evaluationCtx = MutableContext(targetingKey: "ede04e44-463d-40d1-8fc0-b1d6855578d0")
+        let api = OpenFeatureAPI()
+        await api.setProviderAndWait(provider: provider, initialContext: evaluationCtx)
+        let client = api.getClient()
+
+        _ = client.getBooleanDetails(key: "my-flag", defaultValue: false)
+        _ = client.getBooleanDetails(key: "my-flag", defaultValue: false)
+        _ = client.getIntegerDetails(key: "int-flag", defaultValue: 1)
+        _ = client.getDoubleDetails(key: "double-flag", defaultValue: 1.0)
+        _ = client.getStringDetails(key: "string-flag", defaultValue: "default")
+        _ = client.getObjectDetails(key: "object-flag", defaultValue: Value.null)
+
+        let expectation = self.expectation(description: "Waiting for delay")
+
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2.0) { expectation.fulfill() }
+        await fulfillment(of: [expectation], timeout: 3.0)
+
+        XCTAssertEqual(1, mockNetworkService.dataCollectorCallCounter)
+        XCTAssertEqual(6, mockNetworkService.dataCollectorEventCounter)
         
+        do {
+            let httpBodyCollector = mockNetworkService.requests[mockNetworkService.requests.count - 1].httpBody!
+            let decodedStruct = try JSONDecoder().decode(DataCollectorRequest.self, from: httpBodyCollector)
+            let want = [
+                "openfeature": ExporterMetadataValue.bool(true),
+                "provider": ExporterMetadataValue.string("swift")
+            ] as? [String: ExporterMetadataValue]
+            XCTAssertEqual(want, decodedStruct.meta)
+        } catch {
+            XCTFail("Error deserializing: \(error)")
+        }
     }
 
     func testProviderMultipleHookCall() async {
